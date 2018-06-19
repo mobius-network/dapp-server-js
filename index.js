46199c0b2b1f10e560df018c5d9a87a3501cb52c
Timur Ramazanov <charlie.vmk@gmail.com>
2018-06-19 15:00:46 +0600

    Stellar network setting

--- index.js
+++ index.js
@@ -8,13 +8,20 @@ const Mobius = require("@mobius-network/mobius-client-js");
 const dev = pug.compile(require('./dev.pug'));
 
 const mobius = new Mobius.Client();
-mobius.network = StellarSdk.Networks.TESTNET;
 
 const app = express();
 app.enable('strict routing');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Set Stellar network to use, based on webtask meta
+app.use((req, res, next) => {
+  network = req.webtaskContext.meta.NETWORK;
+  mobius.network =
+    network === 'public' ? StellarSdk.Networks.PUBLIC : StellarSdk.Networks.TESTNET;
+  next();
+});
 app.use("/api", require("./api"));
 app.use("/auth", require("./auth"));
 
